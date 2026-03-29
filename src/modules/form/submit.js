dd4b7a910ca5717a923a6c325870bcb6a7dc3d74
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2026-03-29 12:27:59 +0200

    Getting id, name and date for reservation

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 
 const form = document.querySelector("form")
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
 
 // Atual date
@@ -12,6 +13,39 @@ selectedDate.min = inputToday
 
 form.onsubmit = (event) => {
     event.preventDefault()
+    /* console.log("Enviado!!") */
 
-    console.log("Enviado!!")
+    try {
+        // Recuperando nome do cliente
+        const name = clientName.value.trim()
+
+        if (!name) {
+            return alert("Informe nome do cliente!")
+        }
+
+        // Recuperando horário selecionado
+        const hourSelected = document.querySelector(".hour-selected")
+
+        if (!hourSelected) {
+            return alert("Selecione a Hora.")
+        }
+
+        // Recuperando somente a hora.
+        const [hour] = hourSelected.innerText.split(":")
+
+        // Inserir a hora na data.
+        const when = dayjs(selectedDate.value).add(hour, "hour")
+
+        // Gera um id
+        const id = new Date().getTime()
+
+        console.log(
+            id,
+            name,
+            when,
+        )
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento")
+        console.log(error)
+    }
 }
